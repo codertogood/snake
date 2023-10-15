c4b737a0d0d09c033a950227e828f0e34afb26de
jude <judeasherparis@gmail.com>
2023-10-15 16:40:26 -0400

    added score. can go through walls.

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <ncurses.h>
 #include <stdlib.h>
+#include <unistd.h>
 #define body '0'
 #define head '8'
 #define head2 '8'
@@ -10,16 +11,19 @@
 #define empty ' '
 int direction = 3;
 int tail_direction = 3;
+int score = 0;
 void update();
 void display();
 void init_board();
+void quit();
+int crash(char square);
 int tail_directions[700];
 int board[700];
 int langth;
 int main(){
     initscr();
     keypad(stdscr, TRUE);
-    wtimeout(stdscr, 1000);
+    wtimeout(stdscr, 400);
     noecho();
     init_board();
     display();
@@ -55,30 +59,58 @@ void update(){
                 if(board[i-1] == apple){
                     langth += 1;
                     apple_ate = 1;
+                    score += 1;
+                }else if(crash(board[i-1])){
+                    quit();
+                }
+                if(i-1 < 0){
+                    board[i + 699] = head;
+                }else{
+                board[i-1] = head;
                 }
                 board[i] = body;
-                board[i-1] = head;
             }else if(direction == 2){ 
                 if(board[i-50] == apple){
                     langth += 1;
                     apple_ate = 1;
+                    score += 1;
+                }else if(crash(board[i-50])){
+                    quit();
+                }
+                if(i-50 < 0){
+                    board[i - 50 + 699] = head;
+                }else{
+                board[i-50] = head;
                 }
                 board[i] = body;
-                board[i-50] = head2;
             }else if(direction == 3){
                 if(board[i+1] == apple){
                     langth += 1;
                     apple_ate = 1;
+                    score += 1;
+                }else if(crash(board[i+1])){
+                    quit();
                 }
                 board[i] = body;
+               if(i+1 >699){
+                    board[i - 699] = head;
+                }else{
                 board[i+1] = head;
+                }
             }else if(direction == 4){ 
                 if(board[i+50] == apple){
                     langth += 1;
                     apple_ate = 1;
+                    score += 1;
+                }else if(crash(board[i+50])){
+                    quit();
                 }
                 board[i] = body;
-                board[i+50] = head2;
+                if(i+50 > 699){
+                    board[i + 50 - 699] = head;
+                }else{
+                board[i+50] = head;
+                }
             }
             break;   
         }
@@ -105,17 +137,33 @@ void update(){
     for(int i=0; i<700; i+=1){
         if(board[i] == tail||board[i] == tail2){
             if(tail_direction == 1){ 
-                board[i] = empty;
-                board[i-1] = tail;
-            }else if(tail_direction == 2){ 
-                board[i] = empty;
+                 if(i-1 < 0){
+                    board[i + 699] = tail;
+                }else{
+                    board[i-1] = tail;
+                }    
+                board[i] = empty;    
+            }else if(tail_direction == 2){
+                if(i-50 < 0){
+                    board[i - 50 + 699] = tail2;
+                }else{
                 board[i-50] = tail2;
+                }
+                board[i] = empty;
             }else if(tail_direction == 3){ 
-                board[i] = empty;
+                if(i+1 > 699){
+                    board[i - 699] = tail;
+                }else{
                 board[i+1] = tail;
-            }else if(tail_direction == 4){ 
+                }
                 board[i] = empty;
+            }else if(tail_direction == 4){ 
+                if(i+50 > 699){
+                    board[i + 50 - 699] = tail2;
+                }else{
                 board[i+50] = tail2;
+                }
+                board[i] = empty;
             }  
             break;  
         }
@@ -123,10 +171,22 @@ void update(){
 }
 
 // direction 1 = left, derection 2 = up, derection 3 = right, derection 4 = down.
+void quit(){
+    clear();
+    printw("  _____                 ____              \n / ___/__ ___ _  ___   / __ \\_  _____ ____\n/ (_ / _ `/  \' \\/ -_) / /_/ / |/ / -_) __/\n\\___/\\_,_/_/_/_/\\__/  \\____/|___/\\__/_/   \n   \n your score was:%i\n", score);
+    refresh();
+    sleep(5);
+    endwin();
+    exit(0);
+}
+
+int crash(char square){
+    return square == body|| square == tail|| square == tail2;
+}
 
 void display(){
     int index=0;
-    printw(" --------------------------------------------------\n");
+    printw(" --------------------------------------------------  score: %i\n", score);
     for(int i=0; i<14; i+=1){
         printw("|");
         for(int j=0; j<50; j+=1){
